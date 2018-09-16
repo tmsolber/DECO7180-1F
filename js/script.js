5385e2ed5aa62890c0e08b9a421878bece6adcea
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2018-09-16 14:15:25 +1000

    Add clickable moving image

--- js/script.js
+++ js/script.js
@@ -9,7 +9,32 @@ function iterateRecords(data) {
   });
 }
 
+function moveIt() {
+  // Use math.random() to find a new position for the picture
+  picture.style.top = Math.round(Math.random() * spaceW) + 'px';
+  picture.style.left = Math.round(Math.random() * spaceH) + 'px';
+}
+
 $(document).ready(function() {
+  var isPaused = false;
+  var time = 0;
+  picture = $('#animalImage')[0];
+  $('#animalImage').click(function() {
+    isPaused = true;
+
+    alert('This image has been clicked');
+  });
+
+  spaceW = screen.height - picture.height;
+  spaceH = screen.width - picture.width;
+
+  setInterval(function() {
+    if (!isPaused) {
+      time++;
+      moveIt();
+    }
+  }, 5000);
+
   var data = {
     resource_id: '3e39dd7d-e777-4f47-9160-95aaca34bff5'
   };
